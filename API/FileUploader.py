e0a9799ae7ed1d46f2d69932d9a0eb44201c7d24
Aarya-Vakharia <avakhari@usc.edu>
2023-03-28 15:59:18 -0700

    Added Uploading endpoint

---
+++ API/FileUploader.py
@@ -0,0 +1,53 @@
+from flask_restful import Resource
+from constants import UPLOAD_FOLDER
+from flask import request, jsonify
+from werkzeug.utils import secure_filename
+
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+class FileUploader(Resource):
+    def get(self):
+        return "Upload"
+    
+    def post(self):
+        # check if the post request has the file part
+        if 'files' not in request.files:
+            resp = jsonify({'message' : 'No file part in the request'})
+            resp.status_code = 400
+            return resp
+        files = request.files.getlist('files')
+
+        errors = {}
+        success = False
+
+        # Check all files and Upload if no errors 
+        for file in files:
+            print(type(file))
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(UPLOAD_FOLDER + "/" + filename)
+                success = True
+            else:
+                errors[file.filename] = 'File type not allowed'
+             
+
+        if success and errors:
+            errors['message'] = "Files(s) uploaded succesfully"
+            resp = jsonify(errors)
+            resp.status_code = 500
+            return resp
+
+        if success:
+            resp = jsonify({'message':'File(s) Uploaded succesfully'})
+            resp.status_code = 201
+            return resp
+    
+        else:
+            resp = jsonify(errors)
+            resp.status_code = 500
+            return resp
--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 my_file = open("/ifs/loni/faculty/njahansh/nerds/iyad/CPcal_UKBB/Genetics/BioGen/rs2/cpcal_baseline.log", "r")
-print(my_file.read())
+# print(my_file.read())
 
 
 
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS #, cross_origin
 
-from flask_cors import CORS, cross_origin
-
+from constants import UPLOAD_FOLDER
+from API.FileUploader import FileUploader
 
 app = Flask(__name__)
 cors = CORS(app)
+
+# Configurations 
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
 api = Api(app)
 
 # api.add_resource(CohortList,'/cohorts') #Done
+api.add_resource(FileUploader,'/upload')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
 
 
 
