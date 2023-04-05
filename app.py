d325a4cb37b30d0f62c912a6e50dc7b929fd5e47
A2U8C <ankushshetty95@gmail.com>
2023-04-05 01:27:48 -0700

    Updated_code
    With working:
    /file_info
    /analysis_info

--- app.py
+++ app.py
@@ -55,35 +55,4 @@ api.add_resource(CellTypeStats, '/analysis_info')
 # print(HeritVari.post(file_name))
 
 if __name__ == "__main__":  
-    app.run(host="0.0.0.0", debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.run(host="0.0.0.0",port=8080, debug=True)
